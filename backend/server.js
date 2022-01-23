74b866598c9cea6de70c61d47823fef7799ea36b
Mohamad Hanafi <78841380+MohamadHanafi@users.noreply.github.com>
2022-01-23 10:24:31 +0200

    add security layers

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ import dotenv from "dotenv";
 import colors from "colors"; // for console logs
 import morgan from "morgan"; // for logging requests
 
+// security
+import mongoSanitize from "express-mongo-sanitize";
+import helmet from "helmet";
+import xssClean from "xss-clean";
+import hpp from "hpp";
+
 import connectDb from "./config/db.js"; // connect to database
 
 import { notFound, errorHandler } from "./middleware/errorsMiddleware.js"; // error handling middleware;
@@ -20,6 +26,19 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// security middleware
+//Sanitize data
+app.use(mongoSanitize());
+
+// set security headers
+app.use(helmet());
+
+//prevent xss attack
+app.use(xssClean());
+
+//prevent http param pollution
+app.use(hpp());
 
 app.use(express.json());
 
