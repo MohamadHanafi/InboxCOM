51c1f6f0d5a52a312aba3d2b6e1de92e56b8b39f
Mohamad Hanafi <78841380+MohamadHanafi@users.noreply.github.com>
2022-01-23 10:52:39 +0200

    fixed a minor typo

--- backend/routes/MessageRoutes.js
+++ backend/routes/MessageRoutes.js
@@ -9,10 +9,13 @@ import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+// create a message
 router.route("/").post(protect, createMessage);
 
+// get all messages
 router.get("/:user_id", protect, getMessages);
 
+// get single message
 router.get("/:user_id/:message_id", protect, getMessage);
 
 export default router;
