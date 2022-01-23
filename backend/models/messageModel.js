f034d72c5be2250509d74db90eca1b725f423bb3
Mohamad Hanafi <78841380+MohamadHanafi@users.noreply.github.com>
2022-01-23 11:18:55 +0200

    fixed a minor typo

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const messageSchema = mongoose.Schema(
   {
     to: {
-      // email of the user that the message is sent to
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
