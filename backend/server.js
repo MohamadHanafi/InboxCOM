160b38379c6cc95617faf2a354632d775db1b306
Mohamad Hanafi <78841380+MohamadHanafi@users.noreply.github.com>
2022-01-23 10:39:28 +0200

    prepared for publishing

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors"; // for console logs
@@ -45,10 +46,19 @@ app.use(express.json());
 app.use("/api/users", userRouter);
 app.use("/api/messages", messageRouter);
 
-const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+}
 
 app.use(notFound);
 app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`.yellow.bold);
